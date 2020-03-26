491edc17c7f841579efbeb8cd6eaf271251b0984
‘Yu shigui <yu.shigui@rongzer.com>
2020-03-26 17:35:47 +0800

    x

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import styles from './index.less';
-import logo from '../../public/logo.png';
-import iconScp from '../../public/apps/scp.png';
-import iconTimer from '../../public/apps/timer.png';
+import logo from '../assets/logo.png';
+import iconScp from '../assets/apps/scp.png';
+import iconRecognition from '../assets/apps/recognition.png';
 
 class Index extends React.PureComponent {
-
   state = {
     apps: [
-      {id: 1, name: '进销存系统', icon: iconScp, url: 'http://scp.bzdnet.com', description: '简单的产品进销存管理系统'},
-      {id: 2, name: '计时器', icon: iconTimer, url: 'http://timer.bzdnet.com', description: '计时器工具，分正计时和倒计时'},
-    ]
+      {
+        id: 1,
+        name: '进销存',
+        icon: iconScp,
+        url: 'http://scp.bzdnet.com',
+        description: '简单的产品进销存管理系统',
+      },
+      {
+        id: 2,
+        name: '人脸识别',
+        icon: iconRecognition,
+        url: 'http://recognition.bzdnet.com',
+        description: '人脸识别',
+      },
+    ],
   };
 
   render() {
-    const {apps} = this.state;
+    const { apps } = this.state;
     return (
       <div className={styles.pageContainer}>
         <div className={styles.header}>
-          <img className={styles.logo} src={logo}/>
+          <img className={styles.logo} src={logo} />
           <div className={styles.name}>x</div>
         </div>
         <div className={styles.appList}>
@@ -26,11 +37,9 @@ class Index extends React.PureComponent {
             <div key={app.id} className={styles.appContainer}>
               <a target="_blank" href={app.url}>
                 <div className={styles.icon}>
-                  <img src={app.icon}/>
+                  <img src={app.icon} />
                 </div>
-                <div className={styles.name}>
-                  {app.name}
-                </div>
+                <div className={styles.name}>{app.name}</div>
               </a>
               <div className={styles.desc}>{app.description}</div>
             </div>
@@ -39,7 +48,6 @@ class Index extends React.PureComponent {
       </div>
     );
   }
-
 }
 
 export default Index;
