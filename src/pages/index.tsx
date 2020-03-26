be813af94d9a3fd7efe1c3629e5b62a80beb8cb6
‘Yu shigui <yu.shigui@rongzer.com>
2020-03-26 12:35:03 +0800

    feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './index.less';
-import logo from '../assets/logo.png';
+import logo from '../../public/logo.png';
+import iconScp from '../../public/apps/scp.png';
+import iconTimer from '../../public/apps/timer.png';
 
 class Index extends React.PureComponent {
+
   state = {
     apps: [
-      {id: 1, name: 'a', icon: '', url: '', description: 'aaa'},
-      {id: 2, name: 'b', icon: '', url: '', description: 'bbb'},
-      {id: 3, name: 'c', icon: '', url: '', description: 'ccc'},
+      {id: 1, name: '进销存系统', icon: iconScp, url: 'http://scp.bzdnet.com', description: '简单的产品进销存管理系统'},
+      {id: 2, name: '计时器', icon: iconTimer, url: 'http://timer.bzdnet.com', description: '计时器工具，分正计时和倒计时'},
     ]
-  }
+  };
 
   render() {
     const {apps} = this.state;
@@ -22,9 +24,11 @@ class Index extends React.PureComponent {
         <div className={styles.appList}>
           {apps.map(app => (
             <div key={app.id} className={styles.appContainer}>
-              <div className={styles.icon}></div>
+              <div className={styles.icon}>
+                <img src={app.icon}/>
+              </div>
               <div className={styles.name}>
-                <a href={app.url}>{app.name}</a>
+                <a target="_blank" href={app.url}>{app.name}</a>
               </div>
               <div className={styles.desc}>{app.description}</div>
             </div>
