c43c12865978d5a9f1de3a64bfea635bd25f3ec5
‘Yu shigui <yu.shigui@rongzer.com>
2020-03-30 16:27:04 +0800

    x

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index', title: '自由自在的鱼' }],
 });
